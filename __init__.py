0a39106f803da0a3f0ae94ae317470d8c63310aa
jumper047 <jumper047@gmail.com>
2020-03-07 18:39:49 +0300

    remove ugly tabs

--- __init__.py
+++ __init__.py
@@ -23,88 +23,85 @@ from mycroft import intent_file_handler
 from pixel_ring import pixel_ring
 from gpiozero import LED
 
+
 class ReSpeaker_4mic_hat(MycroftSkill):
 
-	def __init__(self):
-		super(ReSpeaker_4mic_hat, self).__init__(name="ReSpeaker_4mic_hat")
+    def __init__(self):
+        super(ReSpeaker_4mic_hat, self).__init__(name="ReSpeaker_4mic_hat")
 
-	def initialize(self):
-		self.log.info("Pixel Ring: Initializing")
-		self.power = LED(5)
-		self.power.on()
-		pixel_ring.set_brightness(10)
-		pixel_ring.change_pattern('echo')
-		pixel_ring.wakeup()
-		self.enable()
+    def initialize(self):
+        self.log.info("Pixel Ring: Initializing")
+        self.power = LED(5)
+        self.power.on()
+        pixel_ring.set_brightness(10)
+        pixel_ring.change_pattern('echo')
+        pixel_ring.wakeup()
+        self.enable()
 
-	def enable(self):
-		self.log.info("Pixel Ring: Enabling")
+    def enable(self):
+        self.log.info("Pixel Ring: Enabling")
+        self.add_event('recognizer_loop:wakeword',
+                       self.handle_listener_wakeup)
+        self.add_event('recognizer_loop:record_begin',
+                       self.handle_listener_on)
+        self.add_event('recognizer_loop:record_end',
+                       self.handle_listener_off)
+        self.add_event('mycroft.skill.handler.start',
+                       self.handle_listener_think)
+        self.add_event('mycroft.skill.handler.complete',
+                       self.handle_listener_off)
+        self.add_event('recognizer_loop:audio_output_start',
+                       self.handler_listener_speak)
+        self.add_event('recognizer_loop:audio_output_end',
+                       self.handle_listener_off)
+        pixel_ring.off()
 
-		self.add_event('recognizer_loop:wakeword',
-				self.handle_listener_wakeup)
-		self.add_event('recognizer_loop:record_begin',
-			       self.handle_listener_on)
-		self.add_event('recognizer_loop:record_end',
-				self.handle_listener_off)
+    def disable(self):
+        self.log.info("Pixel Ring: Disabling")
+        self.remove_event('recognizer_loop:wakeup')
+        self.remove_event('recognizer_loop:record_begin')
+        self.remove_event('recognizer_loop:record_end')
+        self.remove_event('recognizer_loop:audio_output_start')
+        self.remove_event('recognizer_loop:audio_output_end')
+        self.remove_event('mycroft.skill.handler.start')
+        self.remove_event('mycroft.skill.handler.complete')
 
-		self.add_event('mycroft.skill.handler.start',
-				self.handle_listener_think)
-		self.add_event('mycroft.skill.handler.complete',
-				self.handle_listener_off)
+    def shutdown(self):
+        self.log.info("Pixel Ring: Shutdown")
+        pixel_ring.off()
+        self.power.off()
 
-		self.add_event('recognizer_loop:audio_output_start',
-				self.handler_listener_speak)
-		self.add_event('recognizer_loop:audio_output_end',
-				self.handle_listener_off)
+    def handle_listener_wakeup(self, message):
+        self.log.info("Pixel Ring: Wakeup")
+        pixel_ring.wakeup()
+        time.sleep(0.3)
+        pixel_ring.off()
 
-		pixel_ring.off()
+    def handle_listener_on(self, message):
+        self.log.info("Pixel Ring: On")
+        pixel_ring.listen()
 
-	def disable(self):
-		self.log.info("Pixel Ring: Disabling")
-		self.remove_event('recognizer_loop:wakeup')
-		self.remove_event('recognizer_loop:record_begin')
-		self.remove_event('recognizer_loop:record_end')
-		self.remove_event('recognizer_loop:audio_output_start')
-		self.remove_event('recognizer_loop:audio_output_end')
-		self.remove_event('mycroft.skill.handler.start')
-		self.remove_event('mycroft.skill.handler.complete')
+    def handle_listener_off(self, message):
+        self.log.info("Pixel Ring: Off")
+        pixel_ring.off()
 
-	def shutdown(self):
-		self.log.info("Pixel Ring: Shutdown")
-		pixel_ring.off()
-		self.power.off()
+    def handle_listener_think(self, message):
+        self.log.info("Pixel Ring: Think")
+        pixel_ring.think()
 
-	def handle_listener_wakeup(self, message):
-		self.log.info("Pixel Ring: Wakeup")
-		pixel_ring.wakeup()
-		time.sleep(0.3)
-		pixel_ring.off()
-		
-	def handle_listener_on(self, message):
-		self.log.info("Pixel Ring: On")
-		pixel_ring.listen()
-	
-	def handle_listener_off(self, message):
-		self.log.info("Pixel Ring: Off")
-		pixel_ring.off()
+    def handler_listener_speak(self, message):
+        self.log.info("Pixel Ring: Speak")
+        pixel_ring.speak()
 
-	def handle_listener_think(self, message):
-		self.log.info("Pixel Ring: Think")
-		pixel_ring.think()
+    @intent_handler(IntentBuilder("").require("EnablePixelRing"))
+    def handle_enable_pixel_ring_intent(self, message):
+        self.enable()
+        self.speak_dialog("EnablePixelRing")
 
-	def handler_listener_speak(self, message):
-		self.log.info("Pixel Ring: Speak")
-		pixel_ring.speak()
+    @intent_handler(IntentBuilder("").require("DisablePixelRing"))
+    def handle_disable_pixel_ring_intent(self, message):
+        self.disable()
+        self.speak_dialog("DisablePixelRing")
 
-	@intent_handler(IntentBuilder("").require("EnablePixelRing"))
-	def handle_enable_pixel_ring_intent(self, message):
-		self.enable()
-		self.speak_dialog("EnablePixelRing")
-
-	@intent_handler(IntentBuilder("").require("DisablePixelRing"))
-	def handle_disable_pixel_ring_intent(self, message):
-		self.disable()
-		self.speak_dialog("DisablePixelRing")
-
-def create_skill():
-	return ReSpeaker_4mic_hat()
+    def create_skill():
+        return ReSpeaker_4mic_hat()
